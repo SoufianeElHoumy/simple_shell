c68961653d9ab0b8ccfc8fd5ccdd72a1d7dba158
SoufianeElHoumy <elhoumysoufian@gmail.com>
2023-12-13 07:28:06 -0800

    shell

---
+++ shell_exit.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * exit_shell - Exit the shell if the command is "exit".
+ * @command_tokens: The parsed command tokens.
+ * @input_line: The input line.
+ */
+void exit_shell(char **command_tokens, char *input_line)
+{
+    int status;
+
+    if (compare_input_strings(command_tokens[0], "exit") == 0)
+    {
+        if (command_tokens[1] == NULL)
+        {
+            free(command_tokens);
+            free(input_line);
+            exit(0);
+        }
+        else
+        {
+            status = atoi(command_tokens[1]);
+            free(command_tokens);
+            free(input_line);
+            exit(status);
+        }
+    }
+}
---
+++ string_copy_simple.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * copy_string - Copy a string from source to destination.
+ * @destination: The destination buffer for the copied string.
+ * @source: The source string to be copied.
+ * Return: Pointer to the destination buffer.
+ */
+char *copy_string(char *destination, char *source)
+{
+    int index = 0;
+
+    while (*(source + index) != '\0')
+    {
+        *(destination + index) = *(source + index);
+        index++;
+    }
+    *(destination + index) = '\0';
+    return (destination);
+}
