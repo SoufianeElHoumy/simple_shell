26d0b2141e6192744549ce7acf8024d308ebd9c7
SoufianeElHoumy <elhoumysoufian@gmail.com>
2023-12-13 07:40:36 -0800

    shell

---
+++ input_reading.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * read_line - Read a line of input.
+ * @input_line: Pointer to the input buffer.
+ * @size: Size of the input buffer.
+ */
+void read_line(char **input_line, size_t size)
+{
+    if (custom_getline(input_line, &size, stdin) == -1)
+    {
+        if (feof(stdin))
+            exit(EXIT_SUCCESS);
+        else
+            exit(EXIT_FAILURE);
+    }
+}
